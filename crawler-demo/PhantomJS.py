32efd1b55be7daef1945f5e2780c510c91315160
colddew <88028842@qq.com>
2017-09-05 23:03:48 +0800

    add TianXun crawler script

--- crawler-demo/PhantomJS.py
+++ crawler-demo/PhantomJS.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 
 driver = webdriver.PhantomJS()
-driver.implicitly_wait(40)
+driver.implicitly_wait(10)
 driver.get("http://duorou.com/duorou-qiuli-1202.html")
 li = driver.find_element_by_xpath("//dd[@class='goods_w']//li[last()]")
 li.click()
---
+++ crawler/TianXunPySpider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2017-09-05 16:42:49
+# Project: tianxun
+
+from pyspider.libs.base_handler import *
+
+
+class Handler(BaseHandler):
+    crawl_config = {
+    }
